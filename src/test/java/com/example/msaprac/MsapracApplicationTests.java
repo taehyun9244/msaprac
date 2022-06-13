943b07f2bd4efdecefeabf6676dd1895d6ab7f94
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-06-14 03:48:29 +0900

    Initial commit

---
+++ src/test/java/com/example/msaprac/MsapracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsapracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
